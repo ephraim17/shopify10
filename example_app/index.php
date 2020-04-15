a8e4c882b50349bffaaa3a373744a0c53cc66771
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-04-15 17:52:38 +0530

    Update index.php

--- example_app/index.php
+++ example_app/index.php
@@ -1,1 +1,37 @@
-<?php echo "I Am hosted at Heroku"; ?>
+<?php 
+require_once("inc/functions.php");
+
+
+$requests = $_GET;
+$hmac = $_GET['hmac'];
+$serializeArray = serialize($requests);
+$requests = array_diff_key($requests, array( 'hmac' => '' ));
+ksort($requests);
+
+$token = "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";
+$shop = "myheroku";
+
+$collectionList = shopify_call($token, $shop, "/admin/api/2020-04/custom-collections.json", array(), 'GET');
+$collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
+$collection_id = $collectionList['custom_collections'][0]['id'];
+
+$collects = shopify_call($token, $shop, "/admin/api/2020-04/collects.json", array("collection_id"=>$collection_id), "GET");
+$collects = json_decode($collects['response'], JSON_PRETTY_PRINT);
+
+foreach ($collects as $collect) {
+	foreach($collect as $key => $value) {
+		$products = shopify_call($token, $shop, "/admin/api/2020-04/products/" . $value['product_id'] . ".json", array("collection_id"=>$collection_id), "GET");
+		$products = json_decode($products['response'], JSON_PRETTY_PRINT);
+
+
+
+         echo $products['product']['title'] . '<br/>';
+
+
+
+
+	}
+}
+
+
+ ?>
