b0fb1e7f9366b893c30ed95f199c765a0aaa154b
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-04-15 22:36:00 +0530

    Update index.php

--- example_app/index.php
+++ example_app/index.php
@@ -8,7 +8,7 @@ $serializeArray = serialize($requests);
 $requests = array_diff_key($requests, array( 'hmac' => '' ));
 ksort($requests);
 
-$token = "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";
+$token = "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";
 $shop = "myheroku";
 
 //Product and Product Images
@@ -22,8 +22,6 @@ $collection_id = $collectionList['custom_collections'][0]['id'];
 $collects = shopify_call($token, $shop, "/admin/api/2020-04/collects.json", array("collection_id"=>$collection_id), "GET");
 $collects = json_decode($collects['response'], JSON_PRETTY_PRINT);
 
-$count = 0;
-
 foreach ($collects as $collect) {
 	foreach($collect as $key => $value) {
 		$products = shopify_call($token, $shop, "/admin/api/2020-04/products/" . $value['product_id'] . ".json", array(), "GET");
@@ -36,12 +34,40 @@ foreach ($collects as $collect) {
 		 $image = $images['images'][0]['src'];
          $title = $products['product']['title'];
          $count++;
-
-
-
-
 	}
 }
+
+foreach ($theme as $curr_theme) {
+	foreach($curr_theme as $key => $value) {
+		if($value['role'] === 'main') {
+
+			echo "Theme ID: " . $value['id'] . "<br/>";
+			echo "Theme Name: " . $value['name'] . "<br/>";
+
+			$array = array(
+   				"asset" => array(
+ 					"key" => "templates/index.liquid",
+ 					"value" => "Hello Wordl from WeeklyHow"
+   				)
+			);
+
+			$assets = shopify_call($token, $shop, "/admin/api/2020-04/themes/" . $value['id'] . "/assets.json", array(), "PUT");
+		    $assets = json_decode($assets['response'], JSON_PRETTY_PRINT);
+
+		}
+	}
+}
+
+
+$script_array = array(
+ 					"script_tag" => array(
+ 						"event" => "onload",
+ 						"src" => "https://shopifyapp10.herokuapp.com/example_app/scripts/script.js"
+ 					)
+);
+
+$scriptTag = shopify_call($token, $shop, "/admin/api/2020-04/themes/script_tags.json", array(), "POST");
+$scriptTag = json_decode($scriptTag['response'], JSON_PRETTY_PRINT);
 
 
  ?>
