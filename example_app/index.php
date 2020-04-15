60cbd68462f7552339f3d8abe82fd0f8ba39f648
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-04-15 21:39:47 +0530

    Update index.php

--- example_app/index.php
+++ example_app/index.php
@@ -31,8 +31,8 @@ foreach ($collects as $collect) {
 		$images = json_decode($products['response'], JSON_PRETTY_PRINT);
 
 
-		 $image = $images['product']['title'] . '<br/>';
-         $title = $products['product']['title'] . '<br/>';
+		 $image = $images['product']['title'];
+                 $title = $products['product']['title'];
 
 
 
