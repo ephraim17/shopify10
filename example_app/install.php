a3ceb55561e84b41c5ec0dd890dd2f15593dd5fa
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-04-19 19:49:25 +0530

    Update install.php

--- example_app/install.php
+++ example_app/install.php
@@ -3,7 +3,7 @@
 // Set variables for our request
 $shop = $_GET['shop'];
 $api_key = "b5be2222d87feee2e9148858b8b69725";
-$scopes = "read_orders,write_products,read_themes,write_themes,write_script_tags";
+$scopes = "read_customers,write_customers,read_orders,write_products,read_themes,write_themes,write_script_tags";
 $redirect_uri = "https://shopifyapp10.herokuapp.com/example_app/generate_token.php";
 
 // Build install/approval URL to redirect to
