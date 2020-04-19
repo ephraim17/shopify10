11c1014f6c4b965f44042c051b9dc8f9e47d5ac7
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-04-19 18:55:02 +0530

    Update index.php

--- example_app/index.php
+++ example_app/index.php
@@ -1,7 +1,7 @@
 <?php
 
 $username = 'nahbeel';
-$url = 'https://api.github.com/users/treehouse';
+$url = 'https://api.github.com/orgs/thephpleague/members';
 $process = curl_init($url);
 curl_setopt($process, CURLOPT_USERAGENT, $username);
 curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
@@ -9,7 +9,7 @@ $return = curl_exec($process);
 $results = json_decode($return);
 
 foreach ($results as $result) {
-   echo '<img src="' . $result->avatar_url . '" width="9%">'
+   echo '<img src="' . $result->avatar_url . '" width="9%">';
 }
 
 curl_close($process);
