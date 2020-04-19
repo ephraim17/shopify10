74725929de2a94d76e25cc1670b090bc80ad26ee
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-04-19 19:38:14 +0530

    Update index.php

--- example_app/index.php
+++ example_app/index.php
@@ -12,17 +12,6 @@ $token = "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";
 $shop = "myheroku";
 
 
-// Based on 4th video
-$customers = shopify_call($token, $shop, "/admin/api/2020-04/customers.json", array(), "GET");
-$customers = json_decode($customers['response'], JSON_PRETTY_PRINT);
 
-foreach ($customers as $customer) {
-	foreach($customer as $key => $value) {
-
-			echo "Theme ID: " . $value['id'] . "<br/>";
-
-		}
-	}
-}
 
  ?>
