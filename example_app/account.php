9b1528b3dd20a90d5f640d929fe2d83b98e8c1af
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-04-19 22:45:44 +0530

    Create account.php

---
+++ example_app/account.php
@@ -0,0 +1,33 @@
+<?php 
+require_once("inc/functions.php");
+
+
+$requests = $_GET;
+$hmac = $_GET['hmac'];
+$serializeArray = serialize($requests);
+$requests = array_diff_key($requests, array( 'hmac' => '' ));
+ksort($requests);
+
+$token = "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";
+$shop = "myheroku";
+
+// Based on 4th video
+$customers = shopify_call($token, $shop, "/admin/api/2020-04/customers.json", array(), "GET");
+$customers = json_decode($customers['response'], JSON_PRETTY_PRINT);
+
+foreach ($customers as $customer) {
+	foreach($customer as $key => $value) {
+		
+?>
+
+<ul>
+	<li><a href="customer.php?id=<?php echo $value[id] ?>"><?php echo $value[first_name] . ' ' . $value[last_name]; ?></a><br/>
+	<small><?php echo $value[email]; ?></small></li>
+</ul>
+
+	
+<?php
+		}
+	}
+
+ ?>
