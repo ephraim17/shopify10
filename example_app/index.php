85108c0191b27f37494fd00abb8f54d135ec2764
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-04-19 18:48:37 +0530

    Create index.php

---
+++ example_app/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$username = 'nahbeel';
+$url = 'https://api.github.com/users/treehouse';
+$process = curl_init($url);
+curl_setopt($process, CURLOPT_USERAGENT, $username);
+curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+$return = curl_exec($process);
+$results = json_decode($return);
+
+foreach ($results as $result) {
+   echo '<img src="' . $result->avatar_url . '" width="9%">'
+}
+
+curl_close($process);
+
+
+?>
