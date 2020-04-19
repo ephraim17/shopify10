89f9219fe6b7b8d008df1cd4fab96d392c49c731
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-04-19 19:28:24 +0530

    Update index.php

--- example_app/index.php
+++ example_app/index.php
@@ -19,7 +19,7 @@ $customers = json_decode($customers['response'], JSON_PRETTY_PRINT);
 foreach ($customers as $customer) {
 	foreach($customer as $key => $value) {
 
-			echo $value['name'] . "<br/>";
+			echo $value['email'] . "<br/>";
 
 	}
 }
